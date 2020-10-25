ee8fbd38ef12f255525e74df7bf05e281bb697be
IlyaMrz <knpkaa@gmail.com>
2020-10-25 14:43:40 +0300

    commit

---
+++ exercisesAndExamples/exercise2(13.4.1).js
@@ -0,0 +1,54 @@
+//#1 change this function into a ternary and assign it to variable called experiencePoints
+function experiencePoints() {
+    if (winBattle()) {
+        return 10;
+    } else {
+        return 1;
+    }
+}
+
+experiencePoints = winBattle() ? 10 : 1;
+
+//Using this function, answer the questions below:
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break;
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+        case "right":
+            return whatHappens = "you found a river";
+            break;
+        case "left":
+            whatHappens = "you run into a troll";
+            break;
+        default:
+            whatHappens = "please enter a valid direction";
+    }
+    return whatHappens;
+}
+
+//#2 return value when moveCommand("forward"); "you encounter a monster"
+
+
+//#3 return value when moveCommand("back"); "you arrived home"
+
+//#4 return value when moveCommand("right");
+
+//#5 return value when moveCommand("left");
+
+//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
+function WhereIsMoney(choice) {
+    let yourWord;
+    switch (choice) {
+        case "it":
+            yourWord = "you with happiness";
+            break;
+        default:
+            yourWord = "make a valid choice";
+    }
+    return yourWord
+}
