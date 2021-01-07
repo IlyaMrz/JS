e22f260bd3a1520ef3254d46146c3f8fcfc3ee46
morozov ilya <knpkaa@gmail.com>
2021-01-07 18:02:52 +0300

    commit

--- facerecognitionAPI/server.js
+++ facerecognitionAPI/server.js
@@ -3,13 +3,18 @@ const bP = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/Register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profileID = require('./controllers/profileID');
+
 
 const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
       user : 'postgres',
-      password : 'sDAWCs1Z',
+      password : '',
       database : 'smart-brain'
     }
   });
@@ -25,105 +30,19 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send(database.users)
-})
-
-app.post('/signin', (req, res) => {
-    db.select('email','hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if (isValid) {
-            return db.select('*').from('users')
-            .where('email','=', req.body.email)
-            .then( user => {
-                res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        } else {
-            res.status(400).json('wrong credentials!')
-        }
+    db.select('*').from('users')
+    .then(i => {
+        res.json(i)
     })
-    .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    // bcrypt.hash(password, null, null, function (err, hash) {
-    //     console.log(hash)
-    // });
-    // Load hash from your password DB.
-    //     bcrypt.compare("carrot", '$2a$10$21yzmmtCSUM3tO6vrXU9qOKInLG3MkKLK/MDKE6g2cwB/JkmqsGBS', function (err, res) {
-    //         console.log(res)
-    //     });
-    // bcrypt.compare("carrot", '$2a$10$PWcBjObfbKHUsQpO1i8Qoep6XX9jf6Tf/HIUIo2mxntcJaoSBPzXe', function (err, res) {
-    //     console.log(res)
-    // });
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email,
+app.post('/signin', (req, res) => {signin.handlerSignin(req,res,db,bcrypt)})
 
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                    email: loginEmail[0],
-                    name: name,
-                    joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to registerQQ'))
-    
-})
+app.post('/register', (req, res) => {register.handlerRegister(req,res,db,bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-    .then(user => {
-        if (user.length) {
-            res.json(user[0])
-        } else {
-            res.status(400).json('Not found!')
-        }
-        
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
+app.get('/profile/:id', (req, res) => {profileID.handlerProfileID(req, res, db)})
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id','=',id)
-    .increment('entries',1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to increase entries'))
-
-})
-
-
-// bcrypt.hash("bacon", null, null, function(err, hash) {
-//     // Store hash in your password DB.
-// });
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function (err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function (err, res) {
-//     // res = false
-// });
+app.put('/image', (req, res) => {image.handlerImage(req,res,db)})
 
 app.listen(3000, () => {
     console.log('app is running on port 3000.')
