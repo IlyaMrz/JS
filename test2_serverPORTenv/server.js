6b75cfa66a900f69b284a9268c5949eda9903951
morozov ilya <knpkaa@gmail.com>
2021-01-08 18:56:58 +0300

    server port injection

--- test2_serverPORTenv/server.js
+++ test2_serverPORTenv/server.js
@@ -1,8 +1,11 @@
 const app = require('http')
     .createServer((req,res) => res.send('oh hi'));
 
-app.listen(3000, ()=>{
-    console.log('sever listening on port 3000')
+const PORT = process.env.PORT    
+app.listen(PORT, ()=>{
+    console.log(`sever listening on port ${PORT}`);
 });
 
-console.log(process.env)
+console.log(PORT)
+
+// launch in bash "PORT=3000 node server.js"
