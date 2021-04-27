4522d82d6e5e5492eb1ced52e6d0f65f061a3365
morozov ilya <knpkaa@gmail.com>
2021-04-28 01:17:17 +0300

    now removes broken utf8 characters

--- YouTube_Split_Trash_Removal.js
+++ YouTube_Split_Trash_Removal.js
@@ -1,10 +1,12 @@
-// removes from text file trash like empty lines, square brackets and https links
+// Removes from text file trash like empty lines, 
+// square brackets, https links and broken utf8 characters.
 
 var fs = require('fs');
 
 let file = fs.readFileSync('D:\\albumSM\\rawTracks.txt', 'utf8');
 file = file.split('\n').filter(e=>!e.includes('http')).filter(e=>e!=='\r') //?
 file = file.filter(e => !['[',']'].includes(e)).join('') //?
-
+let final = file.replace(/[^\x00-\xFF]/g, ""); //?
 //fs.writeFileSync('Tracks.txt', file)
-fs.writeFileSync('D:\\albumSM\\Tracks.txt', file)
+
+fs.writeFileSync('D:\\albumSM\\Tracks.txt', final)
