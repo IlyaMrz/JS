38a1f35e28d962ad96872aacecc6c0c5829b5869
morozov ilya <knpkaa@gmail.com>
2021-06-04 04:17:47 +0300

    init sort

---
+++ TS/sort/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("we are on !");
+var logMe = function () {
+    console.log("whooo");
+};
+logMe();
---
+++ TS/sort/src/index.ts
@@ -0,0 +1,5 @@
+console.log("we are on !");
+const logMe = (): void => {
+    console.log("whooo");
+};
+logMe();
