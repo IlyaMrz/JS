3aadbb3f9878dadeea917cf288d5be0fb95fc5f2
morozov ilya <knpkaa@gmail.com>
2021-06-05 18:31:16 +0300

    86. linked list

--- TS/sort/src/index.ts
+++ TS/sort/src/index.ts
@@ -1,14 +1,25 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollections } from "./NumbersCollections";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numberCollections = new NumbersCollections([1, 3, 5, -4, 9, 0, 4, -44, 3, 44]);
-const sorter = new Sorter(numberCollections);
-sorter.sort();
+const sorterNum = new Sorter(numberCollections);
+sorterNum.sort();
 
 const charCollection = new CharactersCollection("Hi! you look great!");
-const sorter2 = new Sorter(charCollection);
-sorter2.sort();
+const sorterChar = new Sorter(charCollection);
+sorterChar.sort();
+
+const linkedList = new LinkedList();
+linkedList.add(1234);
+linkedList.add(-33);
+linkedList.add(0);
+linkedList.add(10);
+const sorterLiList = new Sorter(linkedList);
+sorterLiList.sort();
 
 console.log("numbers: ", numberCollections.data);
 console.log("char-s: ", charCollection.data);
+console.log("linked list:");
+linkedList.print();
