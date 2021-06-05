e2635de41a310395774e86a976a0caa20d320e74
morozov ilya <knpkaa@gmail.com>
2021-06-05 04:09:12 +0300

    84

--- TS/sort/build/index.js
+++ TS/sort/build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollections_1 = require("./NumbersCollections");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numberCollections = new NumbersCollections_1.NumbersCollections([1, 3, 5, -4, 9, 0, 4, -44, 3, 44]);
 var sorter = new Sorter_1.Sorter(numberCollections);
 sorter.sort();
-console.log(numberCollections.data);
+var charCollection = new CharactersCollection_1.CharactersCollection("Hi! you look great!");
+var sorter2 = new Sorter_1.Sorter(charCollection);
+sorter2.sort();
+console.log("numbers: ", numberCollections.data);
+console.log("char-s: ", charCollection.data);
--- TS/sort/src/index.ts
+++ TS/sort/src/index.ts
@@ -1,8 +1,14 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollections } from "./NumbersCollections";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numberCollections = new NumbersCollections([1, 3, 5, -4, 9, 0, 4, -44, 3, 44]);
 const sorter = new Sorter(numberCollections);
 sorter.sort();
 
-console.log(numberCollections.data);
+const charCollection = new CharactersCollection("Hi! you look great!");
+const sorter2 = new Sorter(charCollection);
+sorter2.sort();
+
+console.log("numbers: ", numberCollections.data);
+console.log("char-s: ", charCollection.data);
