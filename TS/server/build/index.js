eff4463344460cfb290ef9694064e444a55f14e7
morozov ilya <knpkaa@gmail.com>
2021-06-18 04:49:08 +0300

    basic setup

--- TS/server/build/index.js
+++ TS/server/build/index.js
@@ -4,10 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var loginRoutes_1 = require("./routes.ts/loginRoutes");
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
-app.get("/", function (req, res) {
-    res.send("\n        <div>\n         <h1>H1 there!</h1>\n        </div>\n    ");
-});
-app.listen(3000, function () {
-    console.log("listening on port 3000");
-});
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(loginRoutes_1.router);
+app.listen(3000);
--- TS/server/src/index.ts
+++ TS/server/src/index.ts
@@ -1,15 +1,10 @@
 import express, { Request, Response } from "express";
+import { router } from "./routes.ts/loginRoutes";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send(`
-        <div>
-         <h1>H1 there!</h1>
-        </div>
-    `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
-});
+app.listen(3000);
