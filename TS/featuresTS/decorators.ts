ea60530000c267ac10246a927d7480d42d5a2e75
morozov ilya <knpkaa@gmail.com>
2021-06-20 11:34:52 +0300

    227. parameters Decorator and class Decorators

--- TS/featuresTS/decorators.ts
+++ TS/featuresTS/decorators.ts
@@ -1,14 +1,33 @@
+@classDecorator
 class Boat {
+    @testDecorator
     color: string = "red";
+    @testDecorator
     get formattedColor(): string {
         return `This boat color is ${this.color}`;
     }
 
     @logError("oops boat was sunk. kek") // decorator only executes one time when class defined!
-    pilot(): void {
-        throw new Error();
-        console.log("weeeeeee");
+    pilot(@parameterDecorator speed: string, @parameterDecorator newBoat: boolean): void {
+        if ((speed = "fast")) {
+            console.log("wooow");
+        } else {
+            console.log("nah .... ");
+        }
     }
+}
+
+function classDecorator(constructor: typeof Boat) {
+    console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+    console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+    console.log(target);
+    console.log(key);
 }
 
 function logError(errorMessage: string) {
@@ -25,5 +44,3 @@ function logError(errorMessage: string) {
         };
     };
 }
-
-new Boat().pilot();
