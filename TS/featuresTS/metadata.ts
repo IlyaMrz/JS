f61da4462e913486d0f39f4e5be633766d573f6d
morozov ilya <knpkaa@gmail.com>
2021-06-21 10:32:31 +0300

    v3

--- TS/featuresTS/metadata.ts
+++ TS/featuresTS/metadata.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 
+@printMetadata
 class Plane {
     color: string = "red";
 
@@ -15,8 +16,15 @@ function markFunction(secretInfo: string) {
     };
 }
 
-const secret = Reflect.getMetadata("secret", Plane.prototype, "fly");
-console.log(secret);
+// const secret = Reflect.getMetadata("secret", Plane.prototype, "fly");
+// console.log(secret);
+
+function printMetadata(target: typeof Plane) {
+    for (let key in target.prototype) {
+        const secret = Reflect.getMetadata("secret", target.prototype, key);
+        console.log(secret);
+    }
+}
 
 // const plane = {
 //     color: "grey",
