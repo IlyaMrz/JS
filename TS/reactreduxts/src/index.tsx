507f4da70568019ec94d0c7243240e2626dfa47f
morozov ilya <knpkaa@gmail.com>
2021-06-24 12:07:43 +0300

    256. component state w/o constuctor in ts

--- TS/reactreduxts/src/index.tsx
+++ TS/reactreduxts/src/index.tsx
@@ -4,11 +4,31 @@ import ReactDOM from 'react-dom'
 interface AppProps {
     color?: string
 }
+interface AppState {
+    counter:number
+}
 
-class App extends React.Component<AppProps> {
+class App extends React.Component<AppProps,AppState> {
+    constructor(props:AppProps){
+        super(props)
+
+        this.state = {counter: 0}
+    }
+
+    onIncrement = ():void => {
+        this.setState({counter:this.state.counter + 1})
+    }
+    onDecrement():void {
+        this.setState({counter:this.state.counter - 1})
+    }
+
     render(){
-        return <div> helloooo color is {this.props.color}</div>
+        return <div> 
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement.bind(this)}>\Decrement</button>
+                {this.state.counter}
+        </div>
     }
 }
 
-ReactDOM.render(<App color="red" />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
