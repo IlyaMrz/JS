83a196b71947240a83b6a465d73b4c64b446ffc0
morozov ilya <knpkaa@gmail.com>
2021-06-27 22:41:19 +0300

    35. state managment form

--- epicreact/react-fundamentals/src/exercise/06.js
+++ epicreact/react-fundamentals/src/exercise/06.js
@@ -2,30 +2,49 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {createSemicolonClassElement} from 'typescript'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const [name, setName] = React.useState('none')
+  const [error, setError] = React.useState(false)
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const refValue = React.useRef('')
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  function handlesubmit(e) {
+    e.preventDefault()
+    console.log(e)
+    onSubmitUsername(
+      `\n fn: ${e.target.elements.usernameInput.value} \n ref: ${refValue.current.value} \n stateHook: ${name}`,
+    )
+  }
+
+  function onInputChange(e) {
+    if (e.target.value !== e.target.value.toLowerCase()) {
+      setError(true)
+    } else {
+      setName(e.target.value)
+      setError(false)
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handlesubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          id="usernameInput"
+          ref={refValue}
+          type="text"
+          onChange={onInputChange}
+        />
       </div>
-      <button type="submit">Submit</button>
+      {error ? (
+        <div style={{color: 'red'}}>Should not contain a Capital Letters!!</div>
+      ) : null}
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
