78657845f2e23b2a944bdc45ecb04552f92ff84b
morozov ilya <knpkaa@gmail.com>
2021-07-02 01:43:11 +0300

    99. 6. 6.1 useDebugValue hook

--- epicreact/advanced-react-hooks/src/exercise/06.js
+++ epicreact/advanced-react-hooks/src/exercise/06.js
@@ -3,11 +3,13 @@
 
 import * as React from 'react'
 
+const formatDebugValue = ({query, state}) => `\`${query}\` => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
-
+  React.useDebugValue({query, state}, formatDebugValue)
   React.useEffect(() => {
     let mounted = true
     const mql = window.matchMedia(query)
