863fcd9e672fc2caa8fa7c68a7146fd4edc8bbc7
morozov ilya <knpkaa@gmail.com>
2021-07-01 16:49:14 +0300

    89. 3.1 useContext

--- epicreact/advanced-react-hooks/src/exercise/03.js
+++ epicreact/advanced-react-hooks/src/exercise/03.js
@@ -10,13 +10,21 @@ function CountProvider(props) {
   return <CountContext.Provider value={stateNdValue} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
