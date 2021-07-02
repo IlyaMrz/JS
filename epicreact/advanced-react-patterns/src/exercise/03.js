6ad99d9dfc207f3e65a8807807f921ba1f7ae3cb
morozov ilya <knpkaa@gmail.com>
2021-07-02 23:49:05 +0300

    108. 3_3.1 useContext

--- epicreact/advanced-react-patterns/src/exercise/03.js
+++ epicreact/advanced-react-patterns/src/exercise/03.js
@@ -7,17 +7,18 @@ import {Switch} from '../switch'
 // 🐨 create your ToggleContext context here
 // 📜 https://reactjs.org/docs/context.html#reactcreatecontext
 
+const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
+
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
-  // the value is an object that has `on` and `toggle` on it.
-  return React.Children.map(children, child => {
-    return typeof child.type === 'string'
-      ? child
-      : React.cloneElement(child, {on, toggle})
-  })
+  return (
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
+  )
 }
 
 // 🐨 we'll still get the children from props (as it's passed to us by the
@@ -27,21 +28,31 @@ function Toggle({children}) {
 // your context won't be exposed to the user
 // 💰 `const context = React.useContext(ToggleContext)`
 // 📜 https://reactjs.org/docs/hooks-reference.html#usecontext
-function ToggleOn({on, children}) {
+
+const useToggle = () => {
+  const context = React.useContext(ToggleContext)
+  if (!context) throw new Error('useToggle must be used within a <Toggle />')
+  return context
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
   return on ? children : null
 }
 
 // 🐨 do the same thing to this that you did to the ToggleOn component
-function ToggleOff({on, children}) {
+function ToggleOff({children}) {
+  const {on} = useToggle()
   return on ? null : children
 }
 
 // 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
-function ToggleButton({on, toggle, ...props}) {
+function ToggleButton({...props}) {
+  const {on, toggle} = useToggle()
   return <Switch on={on} onClick={toggle} {...props} />
 }
 
-function App() {
+const App = () => {
   return (
     <div>
       <Toggle>
