f479388cb897761428cdc210b867fe24395383a8
morozov ilya <knpkaa@gmail.com>
2021-07-02 16:55:54 +0300

    105. 2 compound components with props

--- epicreact/advanced-react-patterns/src/exercise/02.js
+++ epicreact/advanced-react-patterns/src/exercise/02.js
@@ -4,29 +4,48 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle(props) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-
+  return React.Children.map(props.children, child => {
+    if (typeof child.type === 'string') {
+      return child
+    }
+    return React.cloneElement(child, {on, toggle})
+  })
   // 🐨 replace this with a call to React.Children.map and map each child in
   // props.children to a clone of that child with the props they need using
   // React.cloneElement.
   // 💰 React.Children.map(props.children, child => {/* return child clone here */})
   // 📜 https://reactjs.org/docs/react-api.html#reactchildren
   // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  // return <Switch on={on} onClick={toggle} />
 }
 
 // 🐨 Flesh out each of these components
 
 // Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
+const ToggleOn = ({on, children}) => {
+  return on ? children : null
+}
 
 // Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
+const ToggleOff = ({on, children}) => {
+  return on ? null : children
+}
 
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleButton = ({on, toggle, ...props}) => (
+  <Switch on={on} onClick={toggle} {...props} />
+)
+
+function MyCustomShow({on, toggle}) {
+  return on ? (
+    <div onClick={toggle}>yeeees</div>
+  ) : (
+    <div onClick={toggle}>sad but nooooo</div>
+  )
+}
 
 function App() {
   return (
@@ -34,7 +53,9 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span> __ Hello</span>
         <ToggleButton />
+        <MyCustomShow />
       </Toggle>
     </div>
   )
