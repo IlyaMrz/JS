4d7836cdb71824ef7096c333493a713765b8ad3d
morozov ilya <knpkaa@gmail.com>
2021-07-04 04:00:57 +0300

    137. moved calculation up on tree to not break memo

--- epicreact/react-performance/src/exercise/03.js
+++ epicreact/react-performance/src/exercise/03.js
@@ -21,8 +21,8 @@ function Menu({
           getItemProps={getItemProps}
           item={item}
           index={index}
-          selectedItem={selectedItem}
-          highlightedIndex={highlightedIndex}
+          isSelected={selectedItem?.id === item.id}
+          isHighlighted={highlightedIndex === index}
         >
           {item.name}
         </ListItem>
@@ -37,12 +37,10 @@ function ListItem({
   getItemProps,
   item,
   index,
-  selectedItem,
-  highlightedIndex,
+  isSelected,
+  isHighlighted,
   ...props
 }) {
-  const isSelected = selectedItem?.id === item.id
-  const isHighlighted = highlightedIndex === index
   return (
     <li
       {...getItemProps({
@@ -59,6 +57,26 @@ function ListItem({
 }
 // 🐨 Memoize the ListItem here using React.memo
 ListItem = React.memo(ListItem)
+// ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+//   // true means do NOT rerender
+//   // false means DO rerender
+
+//   // these ones are easy if any of these changed, we should re-render
+//   if (prevProps.getItemProps !== nextProps.getItemProps) return false
+//   if (prevProps.item !== nextProps.item) return false
+//   if (prevProps.index !== nextProps.index) return false
+//   if (prevProps.selectedItem !== nextProps.selectedItem) return false
+
+//   // this is trickier. We should only re-render if this list item:
+//   // 1. was highlighted before and now it's not
+//   // 2. was not highlighted before and now it is
+//   if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
+//     const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
+//     const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
+//     return wasPrevHighlighted === isNowHighlighted
+//   }
+//   return true
+// })
 
 function App() {
   const forceRerender = useForceRerender()
