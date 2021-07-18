89d81df0247b177b8bdd4b974dba97d7f5b15fde
morozov ilya <knpkaa@gmail.com>
2021-07-18 19:52:55 +0300

    169. 4 form testing with allowing override props

--- epicreact/testing-react-apps/src/__tests__/exercise/04.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/04.js
@@ -7,10 +7,11 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 import faker from 'faker'
 
-function buildLoginForm() {
+function buildLoginForm(overrides) {
   return {
     username: faker.internet.userName(),
     password: faker.internet.password(),
+    ...overrides,
   }
 }
 
