5c3a39405f6f10d15db650377b3d525956212eff
morozov ilya <knpkaa@gmail.com>
2021-07-18 19:56:20 +0300

    169. 4 form testing with '@jackfranklin/test-data-bot'

--- epicreact/testing-react-apps/src/__tests__/exercise/04.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/04.js
@@ -5,15 +5,14 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
