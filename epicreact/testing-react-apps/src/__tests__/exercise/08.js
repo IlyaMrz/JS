4909a5adcc4396e8d1d7a66f42f8351704a4bdba
morozov ilya <knpkaa@gmail.com>
2021-07-19 15:49:26 +0300

    191. 08 - testing custom hooks

--- epicreact/testing-react-apps/src/__tests__/exercise/08.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/08.js
@@ -11,12 +11,32 @@ import useCounter from '../../components/use-counter'
 // capabilities of this hook
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
+function UseCounterHookExample() {
+  const {count, increment, decrement} = useCounter()
+  return (
+    <div>
+      <div>Current count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
 
 test('exposes the count and increment/decrement functions', () => {
   // 🐨 render the component
   // 🐨 get the elements you need using screen
   // 🐨 assert on the initial state of the hook
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  render(<UseCounterHookExample />)
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const message = screen.getByText(/current count/i)
+
+  expect(message).toHaveTextContent('Current count: 0')
+  userEvent.click(increment)
+  expect(message).toHaveTextContent('Current count: 1')
+  userEvent.click(decrement)
+  expect(message).toHaveTextContent('Current count: 0')
 })
 
 /* eslint no-unused-vars:0 */
