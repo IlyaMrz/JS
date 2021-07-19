a8f8daed24bad5df9dab89ed4e21d3d93156c0ef
morozov ilya <knpkaa@gmail.com>
2021-07-19 14:00:26 +0300

    188. 7  testing with context and a custom render method

--- epicreact/testing-react-apps/src/__tests__/exercise/07.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/07.js
@@ -6,17 +6,31 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+function renderWithProviders(ui, {theme = 'light', ...options} = {}) {
+  const Wrapper = ({children}) => (
+    <ThemeProvider value={[theme, () => {}]}>{children}</ThemeProvider>
+  )
+  return render(ui, {wrapper: Wrapper, ...options})
+}
+
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  renderWithProviders(<EasyButton>Easy</EasyButton>)
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  renderWithProviders(<EasyButton>Easy</EasyButton>, {
+    theme: 'dark',
+  })
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
 })
 
 /* eslint no-unused-vars:0 */
