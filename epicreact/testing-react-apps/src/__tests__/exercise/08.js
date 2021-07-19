b14fb2f54a524c86480224d84af1b5bac20e5581
morozov ilya <knpkaa@gmail.com>
2021-07-19 15:55:42 +0300

    191. 08 - testing custom -setup function

--- epicreact/testing-react-apps/src/__tests__/exercise/08.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/08.js
@@ -6,48 +6,37 @@ import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
-function UseCounterHookExample() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
+function setup({initialProps} = {}) {
+  const result = {}
+  function TestComponent(props) {
+    result.current = useCounter(props)
+    return null
+  }
+  render(<TestComponent {...initialProps} />)
+  return result
 }
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
-  // render(<UseCounterHookExample />)
-  // const increment = screen.getByRole('button', {name: /increment/i})
-  // const decrement = screen.getByRole('button', {name: /decrement/i})
-  // const message = screen.getByText(/current count/i)
+  const result = setup()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
 
-  // expect(message).toHaveTextContent('Current count: 0')
-  // userEvent.click(increment)
-  // expect(message).toHaveTextContent('Current count: 1')
-  // userEvent.click(decrement)
-  // expect(message).toHaveTextContent('Current count: 0')
-  let result
-  function TestComponent() {
-    result = useCounter()
-    return null
-  }
-  render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 3}})
+  expect(result.current.count).toBe(3)
+})
+
+test('allows customization of the step', () => {
+  const result = setup({initialProps: {step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
