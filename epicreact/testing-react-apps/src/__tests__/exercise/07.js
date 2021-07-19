928ec62e9b7da603c6ce552d53e250711b804eee
morozov ilya <knpkaa@gmail.com>
2021-07-19 14:03:48 +0300

    188. 7  testing with context and a custom render method - swap @testing-library/react with app test utils

--- epicreact/testing-react-apps/src/__tests__/exercise/07.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/07.js
@@ -2,19 +2,11 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import {render, screen} from 'test/test-utils'
 import EasyButton from '../../components/easy-button'
 
-function renderWithProviders(ui, {theme = 'light', ...options} = {}) {
-  const Wrapper = ({children}) => (
-    <ThemeProvider value={[theme, () => {}]}>{children}</ThemeProvider>
-  )
-  return render(ui, {wrapper: Wrapper, ...options})
-}
-
 test('renders with the light styles for the light theme', () => {
-  renderWithProviders(<EasyButton>Easy</EasyButton>)
+  render(<EasyButton>Easy</EasyButton>, {theme: 'light'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -23,14 +15,39 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', () => {
-  renderWithProviders(<EasyButton>Easy</EasyButton>, {
-    theme: 'dark',
-  })
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
     color: white;
   `)
 })
+
+// function renderWithProviders(ui, {theme = 'light', ...options} = {}) {
+//   const Wrapper = ({children}) => (
+//     <ThemeProvider value={[theme, () => {}]}>{children}</ThemeProvider>
+//   )
+//   return render(ui, {wrapper: Wrapper, ...options})
+// }
+
+// test('renders with the light styles for the light theme', () => {
+//   renderWithProviders(<EasyButton>Easy</EasyButton>)
+//   const button = screen.getByRole('button', {name: /easy/i})
+//   expect(button).toHaveStyle(`
+//     background-color: white;
+//     color: black;
+//   `)
+// })
+
+// test('renders with the dark styles for the dark theme', () => {
+//   renderWithProviders(<EasyButton>Easy</EasyButton>, {
+//     theme: 'dark',
+//   })
+//   const button = screen.getByRole('button', {name: /easy/i})
+//   expect(button).toHaveStyle(`
+//     background-color: black;
+//     color: white;
+//   `)
+// })
 
 /* eslint no-unused-vars:0 */
