d2be7ffd492f72748036aa5c99787ae77f543b31
morozov ilya <knpkaa@gmail.com>
2021-07-19 13:00:22 +0300

    175. 5 mocking HTTP requests - test the unhappy path

--- epicreact/testing-react-apps/src/__tests__/exercise/05.js
+++ epicreact/testing-react-apps/src/__tests__/exercise/05.js
@@ -56,3 +56,15 @@ test(`logging in displays the user's username`, async () => {
 
   expect(screen.getByText(username)).toBeInTheDocument()
 })
+test('omitting the password results in an error', async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  // don't type in the password
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+})
