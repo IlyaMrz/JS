18d31f1566462a6236ac3fa97c74a76d61926e2a
morozov ilya <knpkaa@gmail.com>
2021-08-23 11:54:28 +0300

    generic refactor

--- epicreact/react-suspense/src/exercise/01.js
+++ epicreact/react-suspense/src/exercise/01.js
@@ -9,7 +9,7 @@ import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // 🐨 create a variable called "pokemon" (using let)
-let pokemon
+// let pokemon
 // 💣 delete this now...
 // const pokemon = {
 //   name: 'TODO',
@@ -23,23 +23,47 @@ let pokemon
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
 // 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-let pokemonError
-const pokemonPromise = fetchPokemon('bulbasaur').then(
-  res => (pokemon = res),
-  e => (pokemonError = e),
-)
+// let pokemonError
+// const pokemonPromise = fetchPokemon('bulbasaur').then(
+//   res => (pokemon = res),
+//   e => (pokemonError = e),
+// )
+let pokemonResource = createResource(fetchPokemon('pikachu'))
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
 
+function createResource(promise) {
+  let status = 'pending'
+  let result = promise.then(
+    resolved => {
+      status = 'success'
+      result = resolved
+    },
+    rejected => {
+      status = 'error'
+      result = rejected
+    },
+  )
+  return {
+    read() {
+      if (status === 'pending') throw result
+      if (status === 'error') throw result
+      if (status === 'success') return result
+      throw new Error('This should be impossible')
+    },
+  }
+}
+
 function PokemonInfo() {
+  const pokemon = pokemonResource.read()
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
-  if (pokemonError) {
-    throw pokemonError
-  }
-  if (!pokemon) {
-    throw pokemonPromise
-  }
+  // if (pokemonError) {
+  //   throw pokemonError
+  // }
+  // if (!pokemon) {
+  //   throw pokemonPromise
+  // }
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
   return (
